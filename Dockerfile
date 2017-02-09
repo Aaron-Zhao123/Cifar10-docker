1a83dea287d6dcbe37327104825679744ecb5079
Aaron Zhao <aaron@Aarons-MacBook-Pro.local>
2017-02-09 15:38:07 +0000

    new updates

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,5 @@ COPY cifar10.py /root/cifar10.py
 COPY dataset.py /root/dataset.py
 COPY download.py /root/download.py
 COPY train.py /root/train.py
-COPY tmp_20160130.pkl /root/tmp_20160130.pkl
-COPY 20170205.pkl /root/20170205.pkl
-COPY 20170206.pkl /root/20170206.pkl
+
 CMD ["python", "/root/train.py"]
--- train.py
+++ train.py
@@ -280,7 +280,8 @@ def main():
     # model_name = 'tmp_20160130.pkl'
     # model_name = 'data_sync/test20170203.pkl'
     #model_name = '20170205.pkl'
-    model_name = '20170206.pkl'
+    model_name = './data/20170209.pkl'
+    # model_name = 'data_sync/20170206.pkl'
     # model_name = 'test.pkl'
     # model_name = '../tf_official_docker/tmp.pkl'
     PREV_MODEL_EXIST = 1
